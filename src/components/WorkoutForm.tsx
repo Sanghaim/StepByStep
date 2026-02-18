a69b1c57fdfb5cfb90c58172cd759820ba4ebeb9
Straka <vladimir.strak@gmail.com>
2026-02-18 19:19:18 +0100

    basic workout form

--- src/components/WorkoutForm.tsx
+++ src/components/WorkoutForm.tsx
@@ -8,10 +8,22 @@ export const WorkoutForm = ({ exercises } : {exercises: Array<Exercise>}) => {
   const [caloriesBurned, setCaloriesBurned] = useState<number>(0)
   const [notes, setNotes] = useState<string>('')
   const [selectedExercises, setSelectedExercises] = useState<Array<WorkoutExercise>>([])
+  const [currentExerciseId, setCurrentExerciseId] = useState<string>('')
 
 
 
   return (
-    <h1>Add New Workout</h1>
+    <form>
+      <input type="date" value={date} onChange={(e) => setDate(e.target.value)} />
+      <input type="number" value={duration} onChange={(e) => setDuration(Number(e.target.value))} />
+      <input type="number" value={caloriesBurned} onChange={(e) => setCaloriesBurned(Number(e.target.value))} />
+      <textarea value={notes} onChange={(e) => setNotes(e.target.value)} />
+      <select value={currentExerciseId} onChange={(e) => setCurrentExerciseId(e.target.value)}>
+        <option value=''>Choose an exercise...</option>
+        {exercises.map((exercise) => {
+          return <option key={exercise.id} value={exercise.id}>{exercise.name} - {exercise.muscleGroup.join(', ').trimEnd()}</option>
+        })}
+      </select>
+    </form>
   )
 }
